d826e7656eafc4d26e13c2329d2f74b7ba1e0184
Sarthak Ahuja <sarthakahuja3637@gmail.com>
2019-06-22 11:54:55 +0530

    Forgot password done

---
+++ server.js
@@ -0,0 +1,35 @@
+var express= require('express')
+
+var Mongoose= require('mongoose') //1st
+var UserController = require('./usercontoller')
+var ProductController =  require('./productcontroller')
+var HotelController = require('./hotelcontroller')
+var BodyParser = require('body-parser');
+var Cors=require('cors')
+
+const PORT=7000 || process.env.PORT 
+const MONGOURL="mongodb://test:test1234@ds117729.mlab.com:17729/coerproject"      //2nd
+
+var server= express()
+
+server.use(BodyParser.json())
+server.use(Cors())
+
+server.listen(PORT, function()
+{
+    console.log('Server is running at',PORT)
+    Mongoose.connect(MONGOURL,function(error){ //3rd
+     console.log("Error in Connecting to MOngoDB",error);
+    })
+})
+
+ server.post('/signup',UserController.signup)
+ server.post('/login',UserController.login)
+ server.post('/addproduct',ProductController.addproduct)
+ server.get('/allproducts',ProductController.allproducts) 
+ server.post('/addhotel',HotelController.addhotel)
+ server.get('/allhotel',HotelController.allhotel)
+ server.get('/allusers',UserController.allusers)
+ server.post('/forgotpassword',UserController.forgotpassword)
+ server.post('/deleteaccount',UserController.deleteaccount)
+server.get('/product/:id',ProductController.getProduct)
